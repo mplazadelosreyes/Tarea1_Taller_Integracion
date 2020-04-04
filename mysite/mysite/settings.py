1a2f125a741b611d10883f334013e45cf3d7d4b3
MARIA IGNACIA PLAZA DE LOS REYES <miplazadelosreyes@uc.cl>
2020-04-04 19:30:44 -0300

    arreglando

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l1mr=094gv*56yq_8kh^8)0n%8sp&8x#q@n8y5qifmrdw(@wyj'
+SECRET_KEY='l1mr=094gv*56yq_8kh^8)0n%8sp&8x#q@n8y5qifmrdw(@wyj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mariplarickandmortyapp.herokuapp.com']
 
 
 # Application definition
