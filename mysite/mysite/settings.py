591a33393072591179157f1cf7e44fee86e34cc6
MARIA IGNACIA PLAZA DE LOS REYES <miplazadelosreyes@uc.cl>
2020-04-04 19:11:52 -0300

    arreglando settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,5 +122,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media'
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
