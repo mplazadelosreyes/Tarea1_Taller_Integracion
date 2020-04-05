c623ebd925dd0a2820335e6896773cbe138f7832
MARIA IGNACIA PLAZA DE LOS REYES <miplazadelosreyes@uc.cl>
2020-04-04 21:47:10 -0300

    golaa

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
