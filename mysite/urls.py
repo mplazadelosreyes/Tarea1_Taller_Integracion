058e2dee1ebef1a241ee0deeac2f9ba6191a233a
MARIA IGNACIA PLAZA DE LOS REYES <miplazadelosreyes@uc.cl>
2020-04-04 21:04:22 -0300

    prueba

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,19 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from t1 import views
 
 urlpatterns = [
-    # ex: /t1/
-    path('', views.index, name='index'),
-    # click en index pa ir a episode
-    path('<int:episode_id>/', views.episode, name='episode'),
-    # click en episode al nombre del personaje
-    path('character/<int:character_id>/', views.character, name='character'),
-    # click en las locations
-    path('location/<int:location_id>/', views.location, name='location'),
-    # busqueda
-    path('search/', views.search, name='search'),
+    path('t1/', include('t1.urls')),
     path('admin/', admin.site.urls),
 ]
-
